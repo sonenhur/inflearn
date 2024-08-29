012d1aed0cb1121076ca97fa10d454b4baf7d39b
sonenhur <sonen1125@gmail.com>
2024-08-29 17:37:04 +0900

    20240829

--- java-basic/src/class1/ClassStart1.java
+++ java-basic/src/class1/ClassStart1.java
@@ -10,7 +10,13 @@ public class ClassStart1 {
         int student2Age = 16;
         int student2Grade = 80;
 
+        String student3Name = "학생3";
+        int student3Age = 17;
+        int student3Grade = 70;
+
         System.out.println("이름: " + student1Name + " 나이: " + student1Age + " 성적: " + student1Grade);
         System.out.println("이름: " + student2Name + " 나이: " + student2Age + " 성적: " + student2Grade);
+        System.out.println("이름: " + student3Name + " 나이: " + student3Age + " 성적: " + student3Grade);
+
     }
 }
