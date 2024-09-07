8153fc1200b82992f34da4fdaf70a7f1b352e2ed
sonenhur <148170713+sonenhur@users.noreply.github.com>
2024-09-07 17:15:55 +0900

    Add files via upload

--- java-basic/src/ref/MethodChange1.java
+++ java-basic/src/ref/MethodChange1.java
@@ -3,12 +3,12 @@ package ref;
 public class MethodChange1 {
     public static void main(String[] args) {
         int a = 10;
-        System.out.println("메서드 호출 전 a = " + a);
-        changePrimitive(a);
-        System.out.println("메서드 호출 후 a = " + a);
+        System.out.println("메서드 호출 전: a = " + a);
+        changePrimitve(a);
+        System.out.println("메서드 호출 후: a = " + a);
     }
 
-    static void changePrimitive(int x) {
+    static void changePrimitve(int x) {
         x = 20;
     }
 }
--- java-basic/src/ref/VarChange2.java
+++ java-basic/src/ref/VarChange2.java
@@ -6,21 +6,21 @@ public class VarChange2 {
         dataA.value = 10;
         Data dataB = dataA;
 
-        System.out.println("dataA 참조값= " + dataA);
-        System.out.println("dataB 참조값= " + dataB);
-        System.out.println("dataA value = " + dataA.value);
-        System.out.println("dataB value = " + dataB.value);
+        System.out.println("dataA 참조값=" + dataA);
+        System.out.println("dataB 참조값=" + dataB);
+        System.out.println("dataA.value=" + dataA.value);
+        System.out.println("dataB.value=" + dataB.value);
 
-        // dataA 변경
+        //dataA 변경
         dataA.value = 20;
-        System.out.println("변경 dataA.value = 20");
-        System.out.println("dataA value = " + dataA.value);
-        System.out.println("dataB value = " + dataB.value);
+        System.out.println("변경 dataA.value=20");
+        System.out.println("dataA.value=" + dataA.value);
+        System.out.println("dataB.value=" + dataB.value);
 
-        // dataB 변경
-        dataB.value = 30;
-        System.out.println("변경 dataB.value = 20");
-        System.out.println("dataA value = " + dataA.value);
-        System.out.println("dataB value = " + dataB.value);
+        //dataB 변경
+        dataA.value = 30;
+        System.out.println("변경 datB.value=30");
+        System.out.println("dataA.value=" + dataA.value);
+        System.out.println("dataB.value=" + dataB.value);
     }
 }
