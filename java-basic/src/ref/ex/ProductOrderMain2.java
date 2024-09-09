eadfda9cb9d01624c3af20c01ca2a6bf2a00cae4
sonenhur <sonen1125@gmail.com>
2024-09-09 14:11:33 +0900

    20240909

--- java-basic/src/ref/ex/ProductOrderMain2.java
+++ java-basic/src/ref/ex/ProductOrderMain2.java
@@ -1,16 +1,14 @@
 package ref.ex;
 
+
 public class ProductOrderMain2 {
     public static void main(String[] args) {
         ProductOrder[] orders = new ProductOrder[3];
-
         orders[0] = createOrder("두부", 2000, 2);
         orders[1] = createOrder("김치", 5000, 1);
         orders[2] = createOrder("콜라", 1500, 2);
-
         printOrders(orders);
-        int totalAmount = getTotalAmount(orders);
-        System.out.println("총 결제 금액: " + totalAmount);
+        System.out.println("총 결제 금액: " + getTotalAmount(orders));
     }
 
     static ProductOrder createOrder(String productName, int price, int quantity) {
@@ -30,7 +28,7 @@ public class ProductOrderMain2 {
     static int getTotalAmount(ProductOrder[] orders) {
         int totalAmount = 0;
         for (ProductOrder order : orders) {
-            totalAmount += (order.quantity * order.price);
+            totalAmount += order.quantity * order.price;
         }
         return totalAmount;
     }
