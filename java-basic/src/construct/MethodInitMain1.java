121068997fb65c2dcccb33e4c774f9753ecb7585
sonenhur <sonen1125@gmail.com>
2024-09-09 15:53:22 +0900

    20240909

---
+++ java-basic/src/construct/MethodInitMain1.java
@@ -0,0 +1,20 @@
+package construct;
+
+public class MethodInitMain1 {
+    public static void main(String[] args) {
+        MemberInit member1 = new MemberInit();
+        member1.name = "User1";
+        member1.age = 15;
+        member1.grade = 90;
+
+        MemberInit member2 = new MemberInit();
+        member2.name = "User2";
+        member2.age = 15;
+        member2.grade = 90;
+
+        MemberInit[] members = {member1, member2};
+        for (MemberInit member : members) {
+            System.out.println("이름: " + member.name + ", 나이: " + member.age + ", 성적: " + member.grade);
+        }
+    }
+}
