485ea34086f9f3f4d6f9224baf29727e2f6a67e5
sonenhur <sonen1125@gmail.com>
2024-09-11 16:24:07 +0900

    20240911

--- java-basic/src/construct/MemberConstruct.java
+++ java-basic/src/construct/MemberConstruct.java
@@ -5,6 +5,12 @@ public class MemberConstruct {
     int age;
     int grade;
 
+
+    MemberConstruct(String name, int age) {
+        this(name, age, 0);
+    }
+
+    // 생성자 오버로딩
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name:" + name + " age:" + age + " grade:" + grade);
         this.name = name;
