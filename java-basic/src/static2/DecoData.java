7082932561c35fabd199ba415670d211a9f9f8e8
sonenhur <sonen1125@gmail.com>
2024-09-16 17:50:21 +0900

    20240916

--- java-basic/src/static2/DecoData.java
+++ java-basic/src/static2/DecoData.java
@@ -5,16 +5,16 @@ public class DecoData {
     private static int staticValue;
 
     public static void staticCall() {
-//        instanceValue++; // 인스턴스 변수 접근: 컴파일 에러
-//        instanceMethod(); // 인스턴스 메서드 접근: 컴파일 에러
+//      instanceValue++; // 인스턴스 변수 접근: 컴파일 에러
+//      instanceMethod(); // 인스턴스 메서드 접근: 컴파일 에러
 
         staticValue++; // 정적 변수 접근
         staticMethod(); // 정적 메서드 접근
     }
 
     public void instanceCall() {
-        instanceValue++; // 인스턴스 변수 접근: 컴파일 에러
-        instanceMethod(); // 인스턴스 메서드 접근: 컴파일 에러
+        instanceValue++; // 인스턴스 변수 접근
+        instanceMethod(); // 인스턴스 메서드 접근
 
         staticValue++; // 정적 변수 접근
         staticMethod(); // 정적 메서드 접근
@@ -28,5 +28,4 @@ public class DecoData {
     private static void staticMethod() {
         System.out.println("staticValue = " + staticValue);
     }
-
 }
