a9d2c63b9668199c035c7ff0e93362a08ca481b0
sonenhur <sonen1125@gmail.com>
2024-09-18 17:22:58 +0900

    20240918

--- java-basic/src/extends1/super2/ClassC.java
+++ java-basic/src/extends1/super2/ClassC.java
@@ -2,6 +2,7 @@ package extends1.super2;
 
 public class ClassC extends ClassB {
     public ClassC() {
+        //super(); // Class B에는 기본 생성자가 아닌 생성자를 이미 만들었기 때문에 자바가 기본 생성자를 자동으로 만들어주지 않음
         super(10, 20);
         System.out.println("ClassC 생성자");
     }
