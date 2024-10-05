bcc2f3dcda5852d89e578e0bd2bad848d808ba36
sonenhur <148170713+sonenhur@users.noreply.github.com>
2024-10-05 13:05:40 +0900

    Add files via upload

--- java-basic/src/poly/ex3/AbstractMain.java
+++ java-basic/src/poly/ex3/AbstractMain.java
@@ -1,16 +1,25 @@
-package poly.ex2;
+package poly.ex3;
 
-public class AnimalPolyMain1 {
+public class AbstractMain {
     public static void main(String[] args) {
+
+        //추상 클래스 생성 불가
+        //AbstractAnimal animal = new AbstractAnimal();
+
         Dog dog = new Dog();
         Cat cat = new Cat();
         Cow cow = new Cow();
+
+        cat.sound();
+        cat.move();
+
         soundAnimal(dog);
         soundAnimal(cat);
         soundAnimal(cow);
     }
 
-    private static void soundAnimal(Animal animal) {
+    // 변하지 않는 부분
+    private static void soundAnimal(AbstractAnimal animal) {
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
