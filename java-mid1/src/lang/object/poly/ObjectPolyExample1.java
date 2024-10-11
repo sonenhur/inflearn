161b475fd0bf5be794bf6a1e6b98f62ea5666953
sonenhur <sonen1125@gmail.com>
2024-10-11 20:50:08 +0900

    20241011

---
+++ java-mid1/src/lang/object/poly/ObjectPolyExample1.java
@@ -0,0 +1,14 @@
+package lang.object.poly;
+
+public class ObjectPolyExample1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+    }
+
+    public static void action(Object obj) {
+    }
+}
