956d5b6f610b3acc0d541dffdc7995e461b86e20
sonenhur <sonen1125@gmail.com>
2024-10-17 16:00:16 +0900

    202410117

--- java-mid1/src/lang/immutable/address/RefMain1_2.java
+++ java-mid1/src/lang/immutable/address/RefMain1_2.java
@@ -1,11 +1,12 @@
 package lang.immutable.address;
 
-public class RefMain1_1 {
+public class RefMain1_2 {
     public static void main(String[] args) {
 
         //참조형 변수는 하나의 인스턴스를 공유할 수 있다.
-        Address a = new Address("서울");
-        Address b = a;
+        Address a = new Address("서울"); //x001
+        //사이드이펙트 해결방안: a와 b가 처음부터 다른 인스턴스를 참조하면 된다
+        Address b = new Address("서울"); //x002
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
