64a30778a50e0d034f8983c5bb30e8f3b84df594
sonenhur <sonen1125@gmail.com>
2024-10-25 20:28:26 +0900

    20251025

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -1,8 +1,8 @@
 package static1;
 
 public class Data3 {
-    public String name;
     public static int count; //static
+    public String name;
 
     public Data3(String name) {
         this.name = name;
--- src/static2/DecoData.java
+++ src/static2/DecoData.java
@@ -2,8 +2,8 @@ package static2;
 
 public class DecoData {
 
-    private int instanceValue;
     private static int staticValue;
+    private int instanceValue;
 
     public static void staticCall() {
         //instanceValue++; //인스턴스 변수 접근, compile error
@@ -11,6 +11,10 @@ public class DecoData {
 
         staticValue++; //정적 변수 접근
         staticMethod(); //정적 메서드 접근
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue=" + staticValue);
     }
 
     public void instanceCall() {
@@ -21,12 +25,7 @@ public class DecoData {
         staticMethod(); //정적 메서드 접근
     }
 
-
     private void instanceMethod() {
         System.out.println("instanceValue=" + instanceValue);
-    }
-
-    private static void staticMethod() {
-        System.out.println("staticValue=" + staticValue);
     }
 }
